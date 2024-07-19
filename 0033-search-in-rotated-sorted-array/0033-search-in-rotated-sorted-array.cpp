f7fab50925b25c918edad2206b73d3c605966600
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-07-20 00:11:23 +0500

    Time: 3 ms (59.24%), Space: 14.1 MB (8.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -14,14 +14,12 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0, end = nums.size() - 1;
         int mid = getPivot(nums);
-        if (target >= nums[mid] && target <= nums[end])
-    {
-        start = mid;
-    }
-    else if (target == nums[mid])
-        return mid;
-    else
-        end = mid - 1;
+        if (target >= nums[mid] && target <= nums[end]) {
+            start = mid;
+        } else if (target == nums[mid])
+            return mid;
+        else
+            end = mid - 1;
         while (start <= end) {
             mid = start + (end - start) / 2;
             if (nums[mid] == target) {

1dc0f57f9f56aed166b0cc2fdb03700d15d5c408
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-07-29 01:22:49 +0500

    Time: 0 ms (100%), Space: 12.9 MB (22.42%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,39 +10,37 @@
  */
 class Solution {
 public:
-void recursive(ListNode *prev, ListNode *curr, ListNode *&head)
-{
-    if (curr == NULL){
-        head=prev;
-        return;
+    void recursive(ListNode* prev, ListNode* curr, ListNode*& head) {
+        if (curr == NULL) {
+            head = prev;
+            return;
+        }
+        ListNode* forw = curr->next;
+        curr->next = prev;
+        recursive(curr, forw, head);
     }
-    ListNode* forw = curr->next;
-    curr->next = prev;
-    recursive(curr, forw, head);
-}
-ListNode *reverseList(ListNode *head)
-{
-    if (head == NULL || head->next == NULL)
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        recursive(prev, curr, head);
         return head;
-    ListNode *prev = NULL;
-    ListNode *curr = head;
-    recursive(prev, curr, head);
-    return head;
-}
-// ListNode *reverseList(ListNode *head)
-// {
-//     if (head == NULL || head->next == NULL)
-//         return head;
-//     ListNode *prev = NULL;
-//     ListNode *curr = head;
-//     ListNode *forw = new ListNode(0);
-//     while (forw != NULL)
-//     {
-//         forw = curr->next;
-//         curr->next = prev;
-//         prev = curr;
-//         curr = forw;
-//     }
-//     return prev;
-// }
+    }
+    // ListNode *reverseList(ListNode *head)
+    // {
+    //     if (head == NULL || head->next == NULL)
+    //         return head;
+    //     ListNode *prev = NULL;
+    //     ListNode *curr = head;
+    //     ListNode *forw = new ListNode(0);
+    //     while (forw != NULL)
+    //     {
+    //         forw = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = forw;
+    //     }
+    //     return prev;
+    // }
 };

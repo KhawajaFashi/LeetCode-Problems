fe3f13f8f54c084fa5f12d3cbcb67682040b512e
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-07-31 17:22:26 +0500

    Time: 19 ms (76.6%), Space: 11.3 MB (43.82%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,17 +3,14 @@ public:
     int maxVowels(string s, int k) {
         int count = 0, max = 0;
         for (int i = 0; i < k; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u')
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
                 max++;
         }
         count = max;
         for (int i = k; i < s.length(); i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u')
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
                 count++;
-            if (s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' ||
-                s[i - k] == 'o' || s[i - k] == 'u')
+            if (s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' || s[i - k] == 'o' || s[i - k] == 'u')
                 count--;
             max = max > count ? max : count;
         }

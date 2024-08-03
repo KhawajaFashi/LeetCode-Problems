f47d97aed78d97d3945e523eebbd5e9369455171
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-03 12:56:04 +0500

    Time: 0 ms (100%), Space: 17.3 MB (74.08%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -3,12 +3,6 @@ public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         sort(target.begin(), target.end());
         sort(arr.begin(), arr.end());
-        if (target.size() > arr.size() || target.size() < arr.size())
-            return false;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] != target[i])
-                return false;
-        }
-        return true;
+        return (target==arr);
     }
 };

7450ba97f068527dd829d863e99ab18549104ef5
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-05 14:48:23 +0500

    Time: 94 ms (21.64%), Space: 27 MB (80.98%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     string removeStars(string s) {
         stack<char> st;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == '*'){
-            if (!st.empty())
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '*') {
+                if (!st.empty())
+                    st.pop();
+            } else
+                st.push(s[i]);
+        }
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (!st.empty()) {
+                s[i] = st.top();
                 st.pop();
+            } else
+                s.pop_back();
         }
-        else
-            st.push(s[i]);
-    }
-    int n = s.length();
-    for (int i = 0; i < n; i++)
-    {
-        if (!st.empty()){
-            s[i] = st.top();
-            st.pop();
-        }
-        else
-            s.pop_back();
-    }
-    reverse(s.begin(), s.end());
-    return s;
+        reverse(s.begin(), s.end());
+        return s;
     }
 };

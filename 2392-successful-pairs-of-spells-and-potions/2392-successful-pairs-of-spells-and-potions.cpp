b5baa702707604d0fa1bfb3f7c6652fb4eea9a06
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-07 16:33:18 +0500

    Time: 151 ms (57.71%), Space: 103.7 MB (21.8%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-       sort(potions.begin(), potions.end());
-    vector<int> arr;
-    long long prod;
-    long long temp;
-    int n = potions.size(), s = 0, mid, e = n - 1;
-    for (int i = 0; i < spells.size(); i++)
-    {
-        temp=spells[i];
-        s = 0, e = n - 1;
-        if ((long long)(temp * potions[n - 1]) < success)
-        {
-            arr.push_back(0);
-            continue;
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions,
+                                long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> arr;
+        long long prod;
+        long long temp;
+        int n = potions.size(), s = 0, mid, e = n - 1;
+        for (int i = 0; i < spells.size(); i++) {
+            temp = spells[i];
+            s = 0, e = n - 1;
+            if ((long long)(temp * potions[n - 1]) < success) {
+                arr.push_back(0);
+                continue;
+            }
+            while (s < e) {
+                mid = s + (e - s) / 2;
+                prod = potions[mid] * temp;
+                if (prod < success)
+                    s = mid + 1;
+                else
+                    e = mid;
+            }
+            arr.push_back(n - s);
         }
-        while (s < e)
-        {
-            mid = s + (e - s) / 2;
-            prod = potions[mid] * temp;
-            if (prod < success)
-                s = mid + 1;
-            else
-                e = mid;
-        }
-        arr.push_back(n - s);
-    }
-    return arr;
+        return arr;
     }
 };

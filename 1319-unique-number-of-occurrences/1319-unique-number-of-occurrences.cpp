c107eb085dfa7edd848bdb5dc74395214fb37091
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-18 02:45:16 +0500

    Time: 0 ms (100%), Space: 10.8 MB (91.29%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-    sort(arr.begin(), arr.end());
-    vector<int> arr1;
-    arr1.push_back(1);
-    for (int i = 1, k = 0; i < arr.size(); i++)
-    {
-        if (arr[i] > arr[i - 1])
-        {
-            arr1.push_back(1);
-            k++;
+        sort(arr.begin(), arr.end());
+        vector<int> arr1;
+        arr1.push_back(1);
+        for (int i = 1, k = 0; i < arr.size(); i++) {
+            if (arr[i] > arr[i - 1]) {
+                arr1.push_back(1);
+                k++;
+            } else {
+                arr1[k] = arr1[k] + 1;
+            }
         }
-        else
-        {
-            arr1[k] = arr1[k] + 1;
+        for (int i = 0; i < arr1.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr1[i] == arr1[j])
+                    return false;
+            }
         }
-    }
-    for (int i = 0; i < arr1.size(); i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr1[i] == arr1[j])
-                return false;
-        }
-    }
-    return true;
+        return true;
     }
 };

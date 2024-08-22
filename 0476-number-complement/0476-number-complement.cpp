e986b921bbed8267282b0261f6293e38ad2ceebd
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-22 19:04:25 +0500

    Time: 3 ms (41.07%), Space: 7.5 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     int findComplement(int num) {
+//         int rem;
+//         string s;
+//         while (num != 0) {
+//             rem = num % 2;
+//             num /= 2;
+//             s += !(rem & 1);
+//         }
+//         num = 0;
+//         for (int i = s.length() - 1; i >= 0; i--)
+//             num = num * 2 + s[i];
+//         return num;
+//     }
+// };
 class Solution {
 public:
     int findComplement(int num) {
-        int rem;
-        string s;
-        while (num != 0) {
-            rem = num % 2;
-            num /= 2;
-            s += !(rem & 1);
+        if (num == 0) return 1;
+        
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
         }
-        num = 0;
-        for (int i = s.length() - 1; i >= 0; i--)
-            num = num * 2 + s[i];
-        return num;
+        
+        return ~mask & ~num;
     }
 };

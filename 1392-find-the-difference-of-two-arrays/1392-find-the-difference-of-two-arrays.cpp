25d81f7f47553d6473a64a42dd40f827527532d9
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-24 16:37:50 +0500

    Time: 31 ms (86.96%), Space: 37.1 MB (34.21%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int>s1(nums1.begin(),nums1.end());
-        unordered_set<int>s2(nums2.begin(),nums2.end());
-        vector<vector<int>>ans;
-        vector<int>v;
-        for(auto &x :s1)
+        
+        int n1 = nums1.size();
+        
+        int n2 = nums2.size();
+        
+        // declare two sets
+        
+        unordered_set<int> s1;
+        
+        unordered_set<int> s2;
+        
+        // inbsert elements of nums1 into s1
+        
+        for(int i = 0; i < n1; i++)
+            s1.insert(nums1[i]);
+        
+        // insert elements of nums2 into s2
+        
+        for(int i = 0; i < n2; i++)
+            s2.insert(nums2[i]);
+        
+        vector<int> res1;
+        
+        vector<int> res2;
+        
+        // now traverse over s1 and create res1
+        
+        for(auto x : s1)
         {
-            if(s2.find(x)==s2.end())
-                v.push_back(x);
+            if(s2.count(x) == 0)
+            {
+                res1.push_back(x);
+            }
         }
-        ans.push_back(v);
-        v.clear();
-        for(auto &x :s2)
+        
+        // now traverse over s2 and create res2
+        
+        for(auto x : s2)
         {
-            if(s1.find(x)==s1.end())
-                v.push_back(x);
+            if(s1.count(x) == 0)
+            {
+                res2.push_back(x);
+            }
         }
-        ans.push_back(v);
-        return ans;
-
+        
+        return {res1, res2};
     }
 };

4e546a805750503da7f9fbd238e929a1223c876f
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-25 13:46:27 +0500

    Time: 110 ms (91.13%), Space: 58 MB (78.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,70 +10,70 @@
  */
 class Solution {
 public:
-    ListNode* findMid(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* merge(ListNode* left, ListNode* right) {
-        if (left == NULL)
-            return right;
-        if (right == NULL)
-            return left;
-        ListNode* ans = new ListNode(-1);
-        ListNode* temp = ans;
-        while (left != NULL && right != NULL) {
-            if (left->val < right->val) {
-                temp->next = left;
-                temp = left;
-                left = left->next;
-            } else {
-                temp->next = right;
-                temp = right;
-                right = right->next;
-            }
-        }
-        while (left != NULL) {
-            temp->next = left;
-            temp = left;
-            left = left->next;
-        }
-        while (right != NULL) {
-            temp->next = right;
-            temp = right;
-            right = right->next;
-        }
-        ans = ans->next;
-        return ans;
-    }
+    // ListNode* findMid(ListNode* head) {
+    //     ListNode* slow = head;
+    //     ListNode* fast = head->next;
+    //     while (fast && fast->next) {
+    //         slow = slow->next;
+    //         fast = fast->next->next;
+    //     }
+    //     return slow;
+    // }
+    // ListNode* merge(ListNode* left, ListNode* right) {
+    //     if (left == NULL)
+    //         return right;
+    //     if (right == NULL)
+    //         return left;
+    //     ListNode* ans = new ListNode(-1);
+    //     ListNode* temp = ans;
+    //     while (left != NULL && right != NULL) {
+    //         if (left->val < right->val) {
+    //             temp->next = left;
+    //             temp = left;
+    //             left = left->next;
+    //         } else {
+    //             temp->next = right;
+    //             temp = right;
+    //             right = right->next;
+    //         }
+    //     }
+    //     while (left != NULL) {
+    //         temp->next = left;
+    //         temp = left;
+    //         left = left->next;
+    //     }
+    //     while (right != NULL) {
+    //         temp->next = right;
+    //         temp = right;
+    //         right = right->next;
+    //     }
+    //     ans = ans->next;
+    //     return ans;
+    // }
     ListNode* sortList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) {
-            return head;
+        // if (head == nullptr || head->next == nullptr) {
+        //     return head;
+        // }
+        // ListNode* middle = findMid(head);
+        // ListNode* left = head;
+        // ListNode* right = middle->next;
+        // middle->next = NULL;
+        // left = sortList(left);
+        // right = sortList(right);
+        // ListNode* result = merge(left, right);
+        // return result;
+        ListNode* temp = head;
+        vector<int> arr;
+        while (temp != NULL) {
+            arr.push_back(temp->val);
+            temp = temp->next;
         }
-        ListNode* middle = findMid(head);
-        ListNode* left = head;
-        ListNode* right = middle->next;
-        middle->next = NULL;
-        left = sortList(left);
-        right = sortList(right);
-        ListNode* result = merge(left, right);
-        return result;
-        // ListNode* temp = head;
-        // vector<int> arr;
-        // while (temp != NULL) {
-        //     arr.push_back(temp->val);
-        //     temp = temp->next;
-        // }
-        // sort(arr.begin(), arr.end());
-        // temp = head;
-        // for (int i = 0; i < arr.size(); i++) {
-        //     temp->val = arr[i];
-        //     temp = temp->next;
-        // }
-        // return head;
+        sort(arr.begin(), arr.end());
+        temp = head;
+        for (int i = 0; i < arr.size(); i++) {
+            temp->val = arr[i];
+            temp = temp->next;
+        }
+        return head;
     }
 };

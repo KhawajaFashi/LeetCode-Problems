62e1890af8d6b56f6675b18ac2732be4b17fd923
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-29 22:22:20 +0500

    Time: 6 ms (88.51%), Space: 20.9 MB (41.19%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> stk;
-
-        for (auto ast: asteroids)
-        {
-            while (!stk.empty() && stk.back() > 0 && ast < 0)
-            {
-                int diff = ast + stk.back();
+        stack<int> stk;
+        for (int ast : asteroids) {
+            while (!stk.empty() && stk.top() > 0 && ast < 0) {
+                int diff = ast + stk.top();
                 if (diff > 0)
-                {
                     ast = 0;
-                }
                 else if (diff < 0)
-                {
-                    stk.pop_back();
-                }
-                else
-                {
+                    stk.pop();
+                else {
                     ast = 0;
-                    stk.pop_back();
-                } 
+                    stk.pop();
+                }
             }
             if (ast != 0)
-                stk.push_back(ast);
+                stk.push(ast);
         }
-        return stk;
+        vector<int> arr(stk.size());
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            arr[i] = stk.top();
+            stk.pop();
+        }
+        return arr;
     }
 };

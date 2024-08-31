6d09a85addf2e7c906a273c58b2331f6dc8eb4eb
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-31 14:27:09 +0500

    Time: 0 ms (100%), Space: 10.5 MB (90.59%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,7 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
         for (int i = 0, j = n - 1; i < j; i++) {
-            while (i<j&&nums[j] == val)
+            while (i < j && nums[j] == val)
                 j--;
             if (nums[i] == val) {
                 swap(nums[i], nums[j]);

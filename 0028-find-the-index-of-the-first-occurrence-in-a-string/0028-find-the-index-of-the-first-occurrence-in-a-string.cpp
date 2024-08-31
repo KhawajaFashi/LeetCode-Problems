81008e53f3ac099342e0712c043e44420ba25531
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-08-31 11:38:03 +0500

    Time: 4 ms (16.59%), Space: 8.3 MB (5.94%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int strStr(string haystack, string needle)
     {
         int n = haystack.length(), m = needle.length();
-        for (int i = 0; i < n - m+1; i++)
+        for (int i = 0; i < n - m + 1; i++)
             if (haystack.substr(i, m) == needle)
                 return i;
         return -1;

baa8ca0afc41af60fca2d83d5ba541d5e4c25d8e
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-02 00:28:18 +0500

    Time: 9 ms (21.74%), Space: 8.9 MB (79.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    char convert(char a) { return (a + 32); }
-    string conversion(string s) {
-        string res;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] >= 'A' && s[i] <= 'Z')
-                s[i] = convert(s[i]);
-            if (s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z' ||
-                s[i] >= '0' && s[i] <= '9')
-                res.push_back(s[i]);
-        }
-        return res;
-    }
     bool isPalindrome(string s) {
-        s = conversion(s);
-        int n = s.length(), i = 0, j = n - 1;
-        while (i < j) {
-            if (s[i] != s[j])
+        int start = 0;
+        int end = s.size() - 1;
+        while (start <= end) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[start]) != tolower(s[end]))
                 return false;
-            i++;
-            j--;
+            else {
+                start++;
+                end--;
+            }
         }
         return true;
     }
 };
+// class Solution {
+// public:
+//     char convert(char a) { return (a + 32); }
+//     string conversion(string s) {
+//         string res;
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s[i] >= 'A' && s[i] <= 'Z')
+//                 s[i] = convert(s[i]);
+//             if (s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z' ||
+//                 s[i] >= '0' && s[i] <= '9')
+//                 res.push_back(s[i]);
+//         }
+//         return res;
+//     }
+//     bool isPalindrome(string s) {
+//         s = conversion(s);
+//         int n = s.length(), i = 0, j = n - 1;
+//         while (i < j) {
+//             if (s[i] != s[j])
+//                 return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// };

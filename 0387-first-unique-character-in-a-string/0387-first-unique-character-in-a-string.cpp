e3726eedca470ba80fd93bd7eb5caa05f3a39628
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-02 23:47:27 +0500

    Time: 46 ms (33.26%), Space: 14.4 MB (45.55%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,60 @@
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
+        for (auto a : s)
+            mp[a]++;
+        for (int i = 0; i < s.size(); i++)
+            if (mp[s[i]] == 1)
+                return i;
+        return -1;
+    }
+};
+
+// class Solution {
+// public:
+//     // solution by RITU RANJAN TIWARY
+//     int covertCharIntoIndex(char c) { return 1 + (c - 'a'); }
+//     int firstUniqChar(string s) {
+//         // -2 means repeating char
+//         // -1 means null, just like we consider null in DLL(eg.
+//         // node->prev==null)
+//         int prev[28], next[28], firstOccur[28];
+//         int current, head, tail;
+//         current = head = 0;
+//         tail = 27;
+//         prev[head] = -1;
+//         next[head] = tail;
+//         prev[tail] = head;
+//         next[tail] = -1;
+//         for (int i = 0; i < s.length(); i++) {
+//             int tmp = covertCharIntoIndex(s[i]);
+//             if (firstOccur[tmp] == 0)
+//                 firstOccur[tmp] = i;
+//             if (prev[tmp] == -2 && next[tmp] == -2)
+//                 continue;
+//             // first visit of particular char
+//             if (prev[tmp] == 0 and next[tmp] == 0) {
+//                 prev[tmp] = current;
+//                 next[tmp] = next[current];
+//                 next[current] = tmp;
+//                 current = tmp;
+//             } else {
+//                 if (current == tmp)
+//                     current = prev[tmp];
+//                 int tmpNext = next[tmp];
+//                 int tmpPrev = prev[tmp];
+//                 prev[tmpNext] = tmpPrev;
+//                 next[tmpPrev] = tmpNext;
+//                 prev[tmp] = -2;
+//                 next[tmp] = -2;
+//                 cout << next[head] << endl;
+//             }
+//         }
+//         if (next[head] == 27)
+//             return -1;
+
+//         return firstOccur[next[head]];
+//     }
+// };

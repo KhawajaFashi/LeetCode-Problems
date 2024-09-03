fefff7ab550f4c1b259d261fffcbf0a4705f8681
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-03 23:32:17 +0500

    Time: 0 ms (100%), Space: 10.2 MB (21.06%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        map<char, int> res;
-        string str;
+        // map<char, int> res;
+        string str="";
         int sum = 0;
-        for (int i = 1, j = 48; i <= 26; i++, j++)
-            res[char(j + '1')] = i;
-        for (int i = 0; i < s.length(); i++) 
-            str += to_string(res[s[i]]);
+        // for (int i = 1, j = 48; i <= 26; i++, j++)
+        //     res[char(j + '1')] = i;
+        for (int i = 0; i < s.length(); i++)
+            str +=to_string(s[i] - 'a' + 1); 
+            // str += to_string(res[s[i]]);
         for (int i = 0; i < k; i++) {
             sum = 0;
             for (int j = 0; j < str.length(); j++)

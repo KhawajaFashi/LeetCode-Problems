a15ffc775f83d6d37c06a5fca6fbfa58ac6edde6
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-04 17:13:04 +0500

    Time: 3 ms (39.31%), Space: 8.1 MB (80.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     double myPow(long double x, long n) {
+        // return pow(x, n);
+
+
+        
         // double j = x;
         // if (x == 1)
         //     return x;
@@ -18,6 +22,23 @@ public:
         // } else
         //     return 1.00;
         // return x;
-        return pow(x, n);
+        if(n < 0) {
+            x = 1 / x;
+        } 
+        
+        long num = labs(n);
+        
+        double pow = 1;
+        
+        while(num){ // equivalent to while(num != 0)
+            if(num & 1) { // equivalent to if((num & 1) != 0)
+                pow *= x;
+            }
+            
+            x *= x;
+            num >>= 1;
+        }
+        
+        return pow;
     }
 };

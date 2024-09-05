86ca0f7e91a1a9293650bd2bb8dcbfe79ae6dc03
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-05 19:21:07 +0500

    Time: 116 ms (89.8%), Space: 75.5 MB (16.47%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,15 +1,27 @@
 class ProductOfNumbers {
 public:
-    vector<int> arr;
-    ProductOfNumbers() {}
+    //vector<int> arr;
+    //ProductOfNumbers() {}
 
-    void add(int num) { arr.push_back(num); }
+    // void add(int num) { arr.push_back(num); }
 
+    // int getProduct(int k) {
+    //     int prod = 1;
+    //     for (int i = 0, j = arr.size() - 1; j >= 0 && i < k; i++, j--)
+    //         prod *= arr[j];
+    //     return prod;
+    // }
+    vector<long long> pr = {1};
+    void add(int num) {
+        if (num == 0)
+            pr = {1};
+        else
+            pr.push_back(pr.back() * num);
+    }
     int getProduct(int k) {
-        int prod = 1;
-        for (int i = 0, j = arr.size() - 1; j >= 0 && i < k; i++, j--)
-            prod *= arr[j];
-        return prod;
+        if (k >= pr.size())
+            return 0;
+        return pr.back() / pr[pr.size() - k - 1];
     }
 };
 

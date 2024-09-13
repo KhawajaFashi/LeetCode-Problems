fc28321b440b7d04e3ae1065b679a09a4d124afb
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-13 20:31:05 +0500

    Time: 51 ms (86.85%), Space: 41.6 MB (84.53%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n = arr.size();
-        // vector<int> pre(n);
-        vector<int> ans;
-        // int pre_xor = 0;
-        // for (int xor : arr) {
-        //     pre_xor = xor ^ pre_xor;
-        //     ans.push_back(pre_xor);
-        // }
-        int xoring = 0;
-        for (int i = 0; i < queries.size(); i++) {
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                xoring = xoring ^ arr[j];
-            }
-            ans.push_back(xoring);
-            xoring = 0;
+        vector<int> pre(n);
+        int pre_xor = 0;
+        for (int i = 0; i < n; i++) {
+            pre_xor = arr[i] ^ pre_xor;
+            pre[i] = pre_xor;
         }
-        return ans;
+        vector<int> res(queries.size());
+        for (int k = 0; k < queries.size(); k++) {
+            int i = queries[k][0];
+            int j = queries[k][1];
+            if (i == 0)
+                res[k] = pre[j];
+            else
+                res[k] = pre[j] ^ pre[i - 1];
+        }
+        return res;
     }
 };

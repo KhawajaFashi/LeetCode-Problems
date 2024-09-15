3bd48f058b2b593b1cf8ab81dfd5bb03354ba878
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-09-16 00:07:28 +0500

    Time: 77 ms (51.86%), Space: 18.8 MB (58.84%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        int mask=0;
-        unordered_map<int,int>mp;
-        mp[0]=-1;
+        int mask = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
         int ans = 0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
-                mask = mask^(s[i]-'a'+1);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u') {
+                mask = mask ^ (s[i] - 'a' + 1);
             }
-            if(mp.find(mask)!=mp.end()){
-                ans = max(ans,i-mp[mask]);
-            }
-            else{
+            if (mp.find(mask) != mp.end()) {
+                ans = max(ans, i - mp[mask]);
+            } else {
                 mp[mask] = i;
             }
         }
-        
+
         return ans;
-        
     }
 };
-    // int findTheLongestSubstring(string s) {
-    //     int n = s.length();
-    //     int maxLen = 0;
-    //     map<char, int> mp;
-    //     for (int i = 0; i < n; i++) {
-    //         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-    //             s[i] == 'u')
-    //             mp[s[i]]++;
-    //     }
-    //     map<char, int> mps = mp;
-    //     for (int i = 0; i < n; i++) {
-    //         // cout << mp['o'] << "    " << i << endl;
-    //         for (int j = n - 1; j >= i; j--) {
-    //             if ((mp['a'] % 2 == 0) && (mp['e'] % 2 == 0) &&
-    //                 (mp['i'] % 2 == 0) && (mp['o'] % 2 == 0) &&
-    //                 (mp['u'] % 2 == 0)) {
-    //                 maxLen = max(maxLen, j - i + 1);
-    //                 // cout << i << "       " << j << "      " << j - i << endl;
-    //             }
-    //             mp[s[j]]--;
-    //         }
-    //         mps[s[i]]--;
-    //         mp = mps;
-    //     }
-    //     return maxLen;
-    // }
+// int findTheLongestSubstring(string s) {
+//     int n = s.length();
+//     int maxLen = 0;
+//     map<char, int> mp;
+//     for (int i = 0; i < n; i++) {
+//         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+//             s[i] == 'u')
+//             mp[s[i]]++;
+//     }
+//     map<char, int> mps = mp;
+//     for (int i = 0; i < n; i++) {
+//         // cout << mp['o'] << "    " << i << endl;
+//         for (int j = n - 1; j >= i; j--) {
+//             if ((mp['a'] % 2 == 0) && (mp['e'] % 2 == 0) &&
+//                 (mp['i'] % 2 == 0) && (mp['o'] % 2 == 0) &&
+//                 (mp['u'] % 2 == 0)) {
+//                 maxLen = max(maxLen, j - i + 1);
+//                 // cout << i << "       " << j << "      " << j - i << endl;
+//             }
+//             mp[s[j]]--;
+//         }
+//         mps[s[i]]--;
+//         mp = mps;
+//     }
+//     return maxLen;
+// }

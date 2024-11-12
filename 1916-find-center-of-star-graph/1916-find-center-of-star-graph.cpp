d0e61a97175086e216d22573e6213675a1cd451a
Khawaja <112934339+KhawajaFashi@users.noreply.github.com>
2024-11-12 20:53:23 +0500

    Time: 0 ms (100%), Space: 71 MB (91.52%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+// edges = [[1,2],[5,1],[1,3],[1,4]]
+// edges = [[1,2],[2,3],[4,2]]
     int findCenter(vector<vector<int>>& edges) {
-        int first = 0, second = 0, frequent;
-        for (auto i : edges) {
-            for (auto j : i) {
-                if (j == edges[0][0])
-                    first++;
-                else if (j == edges[0][1])
-                    second++;
-            }
-        }
-        if (first > second)
-            frequent = edges[0][0];
-        else
-            frequent = edges[0][1];
-        return frequent;
+        // int first = 0, second = 0, frequent;
+        // for (auto i : edges) {
+        //     for (auto j : i) {
+        //         if (j == edges[0][0])
+        //             first++;
+        //         else if (j == edges[0][1])
+        //             second++;
+        //     }
+        // }
+        // if (first > second)
+        //     frequent = edges[0][0];
+        // else
+        //     frequent = edges[0][1];
+        // return frequent;
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
     }
 };

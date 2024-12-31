c9e048db9c0f0ad10da12946b46e3cc3d7136415
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2024-12-31 21:08:24 +0500

    Time: 2 ms (81.44%), Space: 14.7 MB (36.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,14 +4,10 @@ public:
         unordered_map<int, int> mp;
         int i = 0;
         for (int num : nums) {
-            mp[num] = i;
-            i++;
-        }
-        i = 0;
-        for (int num : nums) {
-            if (mp.contains(target - num) && mp[target - num] != i) {
+            if (mp.contains(target - num)) {
                 return {i, mp[target - num]};
             }
+            mp[num] = i;
             i++;
         }
         return {};

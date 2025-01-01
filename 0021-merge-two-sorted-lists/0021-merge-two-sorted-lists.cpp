61dd5c62b0879bf0c38e13d3aab6fdd9c9dfd3da
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-01 22:53:38 +0500

    Time: 0 ms (100%), Space: 19.6 MB (44.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,35 +11,50 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* curr1 = list1;
-        ListNode* list3 = new ListNode(0);
-        ListNode* curr2 = list2;
-        if (!curr2)
+        if (!list2)
             return list1;
-        if (!curr1)
+        if (!list1)
             return list2;
-        ListNode* curr3 = list3;
 
-        while (curr1 && curr2) {
-            if (curr1->val >= curr2->val) {
-                curr3->next = curr2;
-                curr2 = curr2->next;
-            } else {
-                curr3->next = curr1;
-                curr1 = curr1->next;
-            }
-            curr3 = curr3->next;
+        if (list1->val >= list2->val) {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-        while (curr2) {
-            curr3->next = curr2;
-            curr2 = curr2->next;
-            curr3 = curr3->next;
+        else {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
-        while (curr1) {
-            curr3->next = curr1;
-            curr1 = curr1->next;
-            curr3 = curr3->next;
-        }
-        return list3->next;
     }
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    //     ListNode* curr1 = list1;
+    //     ListNode* list3 = new ListNode(0);
+    //     ListNode* curr2 = list2;
+    //     if (!curr2)
+    //         return list1;
+    //     if (!curr1)
+    //         return list2;
+    //     ListNode* curr3 = list3;
+
+    //     while (curr1 && curr2) {
+    //         if (curr1->val >= curr2->val) {
+    //             curr3->next = curr2;
+    //             curr2 = curr2->next;
+    //         } else {
+    //             curr3->next = curr1;
+    //             curr1 = curr1->next;
+    //         }
+    //         curr3 = curr3->next;
+    //     }
+    //     while (curr2) {
+    //         curr3->next = curr2;
+    //         curr2 = curr2->next;
+    //         curr3 = curr3->next;
+    //     }
+    //     while (curr1) {
+    //         curr3->next = curr1;
+    //         curr1 = curr1->next;
+    //         curr3 = curr3->next;
+    //     }
+    //     return list3->next;
+    // }
 };

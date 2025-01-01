f2b8afaa32edc61509693f4f20fd51fe823c193a
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-01 22:51:51 +0500

    Time: 2 ms (32.87%), Space: 97.3 MB (38.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = prices[0], profit = 0, buy_index = 0;
+        int mini = INT_MAX;
+        int maxi = 0;
         for (int i = 0; i < prices.size(); i++) {
-            if (buy > prices[i]) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit)
-                profit = prices[i] - buy;
+            int profit = prices[i] - mini;
+            maxi = profit > maxi ? profit : maxi;
+            mini = prices[i] < mini ? prices[i] : mini;
         }
-        return profit;
+        return maxi;
     }
 };

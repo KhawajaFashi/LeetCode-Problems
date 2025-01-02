924b54c202368f2889bdf0b242992938f6d659f7
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-02 13:24:56 +0500

    Time: 0 ms (100%), Space: 12.4 MB (35.8%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -13,15 +13,11 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // Base Case
-        if (root == NULL)
-            return NULL;
-        invertTree(root->left);  // Call the left substree
-        invertTree(root->right); // Call the right substree
-        // Swap the nodes
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        return root; // Return the root
+        if (!root)
+            return root;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left, root->right);
+        return root;
     }
 };

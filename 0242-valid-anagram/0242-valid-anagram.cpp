7be02caf47224f7e5810b3d19b6b1e556c1fabf6
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-02 13:38:04 +0500

    Time: 3 ms (60.6%), Space: 9.5 MB (70.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mp;
-        if (s.length() != t.length())
-            return false;
-        for (char c : s)
-            mp[c]++;
-        for (char c : t) {
-            if (mp.count(c) > 0)
-                mp[c] -= 1;
-            if (mp[c] == 0)
-                mp.erase(c);
-        }
-        if (mp.size())
-            return false;
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+        // map<char, int> mp;
+        // if (s.length() != t.length())
+        //     return false;
+        // for (char c : s)
+        //     mp[c]++;
+        // for (char c : t) {
+        //     if (mp.count(c) > 0)
+        //         mp[c] -= 1;
+        //     if (mp[c] == 0)
+        //         mp.erase(c);
+        // }
+        // if (mp.size())
+        //     return false;
+        // return true;
     }
 };

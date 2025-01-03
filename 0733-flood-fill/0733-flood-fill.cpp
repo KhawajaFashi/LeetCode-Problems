9e4abb97b2ed0266852d34ce71eb46077e4319fb
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-03 22:45:21 +0500

    Time: 0 ms (100%), Space: 18.1 MB (38.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
+    void fill(vector<vector<int>>& image, int sr, int sc, int color,
+              int first) {
+        if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() ||
+            image[sr][sc] == color || image[sr][sc] != first)
+            return;
+        image[sr][sc] = color;
+        fill(image, sr + 1, sc, color, first);
+        fill(image, sr - 1, sc, color, first);
+        fill(image, sr, sc + 1, color, first);
+        fill(image, sr, sc - 1, color, first);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int color) {
         int first_value = image[sr][sc];
-        set<pair<int, int>> s;
-        s.insert({sr, sc});
-        while (!s.empty()) {
-            pair<int, int> p = *(s.begin());
-            int i = p.first;
-            int j = p.second;
-            if (s.count({i - 1, j}) == 0 && i - 1 >= 0 &&
-                image[i - 1][j] != color && image[i - 1][j] == first_value)
-                s.insert({i - 1, j});
-            if (s.count({i + 1, j}) == 0 && i + 1 < image.size() &&
-                image[i + 1][j] != color && image[i + 1][j] == first_value)
-                s.insert({i + 1, j});
-            if (s.count({i, j + 1}) == 0 && j + 1 < image[0].size() &&
-                image[i][j + 1] != color && image[i][j + 1] == first_value)
-                s.insert({i, j + 1});
-            if (s.count({i, j - 1}) == 0 && j - 1 >= 0 &&
-                image[i][j - 1] != color && image[i][j - 1] == first_value)
-                s.insert({i, j - 1});
-            image[i][j] = color;
-            s.erase({i, j});
-        }
+        fill(image, sr, sc, color, first_value);
+        // set<pair<int, int>> s;
+        // s.insert({sr, sc});
+        // while (!s.empty()) {
+        //     pair<int, int> p = *(s.begin());
+        //     int i = p.first;
+        //     int j = p.second;
+        //     if (s.count({i - 1, j}) == 0 && i - 1 >= 0 &&
+        //         image[i - 1][j] != color && image[i - 1][j] == first_value)
+        //         s.insert({i - 1, j});
+        //     if (s.count({i + 1, j}) == 0 && i + 1 < image.size() &&
+        //         image[i + 1][j] != color && image[i + 1][j] == first_value)
+        //         s.insert({i + 1, j});
+        //     if (s.count({i, j + 1}) == 0 && j + 1 < image[0].size() &&
+        //         image[i][j + 1] != color && image[i][j + 1] == first_value)
+        //         s.insert({i, j + 1});
+        //     if (s.count({i, j - 1}) == 0 && j - 1 >= 0 &&
+        //         image[i][j - 1] != color && image[i][j - 1] == first_value)
+        //         s.insert({i, j - 1});
+        //     image[i][j] = color;
+        //     s.erase({i, j});
+        // }
         return image;
     }
 };

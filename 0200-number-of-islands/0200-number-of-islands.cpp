5837b9ae437082dc29af23a2fa7e78f599cc878e
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-05 16:13:00 +0500

    Time: 114 ms (6.28%), Space: 30.3 MB (10.04%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        queue<pair<int, int>> q;
+        map<pair<int, int>, bool> visited;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++)
+                visited[{i, j}] = false;
+        }
+        int islands = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1' && visited[{i, j}] == false) {
+                    q.push({i, j});
+                    islands++;
+                }
+                while (!q.empty()) {
+                    auto element = q.front();
+                    int i = element.first;
+                    int j = element.second;
+                    q.pop();
+                    if (visited[element])
+                        continue;
+                    if (i + 1 < grid.size() && grid[i + 1][j] == '1' &&
+                        !visited[{i + 1, j}])
+                        q.push({i + 1, j});
+                    if (i - 1 >= 0 && grid[i - 1][j] == '1' &&
+                        !visited[{i - 1, j}])
+                        q.push({i - 1, j});
+                    if (j + 1 < grid[i].size() && grid[i][j + 1] == '1' &&
+                        !visited[{i, j + 1}])
+                        q.push({i, j + 1});
+                    if (j - 1 >= 0 && grid[i][j - 1] == '1' &&
+                        !visited[{i, j - 1}])
+                        q.push({i, j - 1});
+                    visited[element] = true;
+                }
+            }
+        }
+        return islands;
+    }
+};

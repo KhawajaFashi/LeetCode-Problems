cea6671f8bbb4ef2b7ac50cf406bc365f80d80bc
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-05 13:01:47 +0500

    Time: 0 ms (100%), Space: 11.8 MB (50.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> alpha(26, 0);
-        for( auto a: ransomNote){
+        for (auto a : magazine) {
             int index = int(a - 'a');
             alpha[index]++;
         }
-        for (auto a : magazine)
-        {
+        for (auto a : ransomNote) {
             int index = int(a - 'a');
-            if(alpha[index]>=1)
-                alpha[index]--;
-        }
-        for(auto a:alpha){
-            if(a>=1)
+            if (alpha[index] == 0)
                 return false;
+            alpha[index]--;
         }
         return true;
     }

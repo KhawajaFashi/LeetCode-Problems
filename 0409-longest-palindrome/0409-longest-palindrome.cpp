df89832293bc3da086d9a14b64bbe506e248d27c
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-06 22:57:17 +0500

    Time: 1 ms (32.5%), Space: 9.2 MB (57.13%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,25 +2,39 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         map<char, int> mp;
-        int count = 0;
-        bool check = false;        
-        for (auto c : s) 
+        int odd_count = 0;
+        for (auto c : s)
+        {
             mp[c]++;
-        for (auto c : s) {
-            if (mp[c] % 2 == 0){
-                count++;
-            }
-            else if (mp[c] > 2) {
-                count++;
-                if (check)
-                    count--;
-                check = true;
-                mp[c]--;
-            } else if (mp[c] == 1 && !check) {
-                count++;
-                check = true;
-            }
+            if (mp[c] % 2 == 1)
+                odd_count++;
+            else
+                odd_count--;
         }
-        return count;
+        if (odd_count > 1)
+            return s.length() - odd_count + 1;
+
+        return s.length();
+        // map<char, int> mp;
+        // int count = 0;
+        // bool check = false;        
+        // for (auto c : s) 
+        //     mp[c]++;
+        // for (auto c : s) {
+        //     if (mp[c] % 2 == 0){
+        //         count++;
+        //     }
+        //     else if (mp[c] > 2) {
+        //         count++;
+        //         if (check)
+        //             count--;
+        //         check = true;
+        //         mp[c]--;
+        //     } else if (mp[c] == 1 && !check) {
+        //         count++;
+        //         check = true;
+        //     }
+        // }
+        // return count;
     }
 };

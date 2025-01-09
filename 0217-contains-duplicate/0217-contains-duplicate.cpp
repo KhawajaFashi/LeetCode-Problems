197f295597f69004630cc62fdb5be9d5e613a5d6
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-09 22:10:29 +0500

    Time: 0 ms (100%), Space: 61 MB (98.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] == nums[i + 1])
-                return true;
+        int n = nums.size();
+        int mini = nums[0];
+        int maxi = nums[0];
+
+        for(int i = 1; i<n;i++)
+        {
+            if(mini < nums[i] || maxi > nums[i])
+            {
+                mini = mini < nums[i] ? nums[i] : mini;
+                maxi = maxi > nums[i] ? nums[i] : maxi;
+                continue;
+            }
+
+            for(int j = 0; j<i; j++)
+            {
+                if(nums[j] == nums[i])
+                {
+                    return true;
+                }
+            }
         }
         return false;
+        // sort(nums.begin(), nums.end());
+        // for (int i = 0; i < nums.size() - 1; i++) {
+        //     if (nums[i] == nums[i + 1])
+        //         return true;
+        // }
+        // return false;
     }
 };

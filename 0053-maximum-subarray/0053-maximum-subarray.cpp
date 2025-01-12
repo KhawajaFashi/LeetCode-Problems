78813ae2113a35f589a77b6057023381faaacf0e
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-12 23:14:05 +0500

    Time: 0 ms (100%), Space: 71.7 MB (65.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, maximum = INT_MIN;
-        bool check = true;
-        for (int num : nums) {
-            if (num > 0)
-                check = false;
-        }
-        for (int num : nums) {
-            if (check) {
-                maximum = maximum < num ? num : maximum;
-                continue;
-            }
-            if (sum + num < 1)
-                sum = 0;
-            else
-                sum += num;
-            maximum = maximum < sum ? sum : maximum;
-        }
-        return maximum;
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
     }
 };

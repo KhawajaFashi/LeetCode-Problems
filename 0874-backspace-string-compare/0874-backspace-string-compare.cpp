0d640b80b02c3b20679a150c625f8e0e76aeecd8
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-12 22:14:17 +0500

    Time: 0 ms (100%), Space: 8.6 MB (26.85%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         int s_len = s.length();
-        for (int i = 0; i < t.length(); i++) {
-            if (t[i] == '#' && i != 0 && s.length() > s_len) {
-                s.pop_back();
-                continue;
-            } else if (t[i] == '#')
-                continue;
-            s += t[i];
-        }
-        t.clear();
-        for (int i = s_len; i < s.length(); i++)
-            t += s[i];
         int t_len = t.length();
         for (int i = 0; i < s_len; i++) {
-            if (s[i] == '#' && i != 0 && t.length() > t_len) {
+            if (s[i] == '#' && s.length() > s_len) {
+                s.pop_back();
+                continue;
+            } else if (s[i] != '#')
+                s += s[i];
+        }
+        s = s.substr(s_len, s.length());
+        for (int i = 0; i < t_len; i++) {
+            if (t[i] == '#' && t.length() > t_len) {
                 t.pop_back();
                 continue;
-            } else if (s[i] == '#')
-                continue;
-            t += s[i];
+            } else if (t[i] != '#')
+                t += t[i];
         }
-        s.clear();
-        for (int i = t_len; i < t.length(); i++)
-            s += t[i];
-        for (int i = t.length() - t_len; i > 0; i--)
-            t.pop_back();
+        t = t.substr(t_len, t.length());
         return t == s;
     }
 };

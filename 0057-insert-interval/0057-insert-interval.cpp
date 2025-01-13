4bb5ccfd7669e5c9fbef9e200ab93689ed851e6e
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-01-13 22:44:06 +0500

    Time: 3 ms (33.81%), Space: 22.2 MB (10.46%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,36 +5,18 @@ public:
         int i = 0;
         int n = intervals.size();
         vector<vector<int>> res;
-        while (i < n) {
+        while (i < n && newI[0] > intervals[i][1]) {
             auto num = intervals[i];
-            if (newI[0] > num[1])
-                res.push_back(num);
-            else
-                break;
+            res.push_back(num);
             i++;
         }
-        while (i < n) {
+        while (i < n && newI[1] >= intervals[i][0]) {
             auto num = intervals[i];
-            if (i < n - 1 && newI[1] < intervals[i + 1][0]) {
-                if (newI[1] < num[0]) {
-                    res.push_back(newI);
-                    break;
-                }
-                newI[0] = min(num[0], newI[0]);
-                newI[1] = max(num[1], newI[1]);
-                res.push_back(newI);
-                i++;
-                break;
-            } else if (num[0] > newI[1]) {
-                res.push_back(newI);
-                break;
-            }
             newI[0] = min(num[0], newI[0]);
             newI[1] = max(num[1], newI[1]);
             i++;
         }
-        if (i == n)
-            res.push_back(newI);
+        res.push_back(newI);
         while (i < n) {
             auto num = intervals[i];
             res.push_back(num);

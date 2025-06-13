7e6343aa89c0d69c70f53374dce2cb06e985b7fa
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-06-13 05:39:54 +0500

    Time: 0 ms (100%), Space: 10.1 MB (42.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0;
-        string newstr;
-        for (char c : s) {
-            if (c >= 65 && c <= 90) {
-                s[i] += 32;
-                newstr += s[i];
-            } else if (c >= 97 && c <= 122 || c >= '0' && c <= '9') {
-                newstr += c;
-            }
-            i++;
-        }
-        for (int i = 0, j = newstr.size() - 1; i <= j; i++, j--) {
-            if (newstr[i] != newstr[j])
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isalnum(s[i]))
+                i++;
+            else if (!isalnum(s[j]))
+                j--;
+            else if (i < j && tolower(s[i]) != tolower(s[j]))
                 return false;
+            else {
+                i++;
+                j--;
+            }
         }
         return true;
     }

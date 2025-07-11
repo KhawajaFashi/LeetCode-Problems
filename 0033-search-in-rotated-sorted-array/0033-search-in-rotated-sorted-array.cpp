7c35b3f14a07545ce11cd006030a8e9dc1bbb7f3
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-07-11 20:18:34 +0500

    Time: 0 ms (100%), Space: 15.2 MB (33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int getPivot(vector<int> nums) {
-        int start = 0, end = nums.size() - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] >= nums[0])
-                start = mid + 1;
-            else
-                end = mid;
-        }
-        return end;
-    }
     int search(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int mid = getPivot(nums);
-        if (target >= nums[mid] && target <= nums[end]) {
-            start = mid;
-        } else if (target == nums[mid])
-            return mid;
-        else
-            end = mid - 1;
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+        int low = 0, high = nums.size() - 1, mid = low + (high - low) / 2;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            int res = nums[mid], low_num = nums[low], high_num = nums[high];
+            if (res == target)
                 return mid;
+            if (res >= low_num) {
+                if (low_num <= target && target <= res)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+
+            } else {
+                if (res <= target && target <= high_num)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else if (nums[mid] < target)
-                start = mid + 1;
         }
         return -1;
     }

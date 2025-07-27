f757d027633034a231ef1dc115681de3cebdee82
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-07-27 12:37:59 +0500

    Time: 0 ms (100%), Space: 9.2 MB (56.37%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.length();
-        long double ans = 0, j = 0;
-        bool check = false;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '0' && ans == 0) {
-                j++;
+        long double result = 0;
+        bool digit = false, neg = false, sign = false;
+        for (char c : s) {
+            if ((c == ' ' && !digit)&&!sign)
+                continue;
+            else if ((c == '+' && !digit) && !sign) {
+                sign = true;
+                continue;
+            } else if (c == '0' && !digit) {
+                digit = true;
+                continue;
+            } else if (c == '-' && !digit && !sign) {
+                neg = true;
+                sign = true;
                 continue;
             }
-            if (s[i] == ' ' && j == 0)
-                continue;
-            if (s[i] == '-' && ans == 0 && j == 0) {
-                j++;
-                check = true;
-                continue;
-            }
-            if (s[i] == '+' && ans == 0 && j == 0) {
-                j++;
-                continue;
-            }
-            if (s[i] >= '0' && s[i] <= '9') {
-                j++;
-                int value = s[i] - '0';
-                if (check == true) {
-                    ans = ans * 10 - value;
-                } else
-                    ans = ans * 10 + value;
-            } else
+            else if (!isdigit(c))
                 break;
+            result = result * 10 + (int(c) - 48);
+            digit = true;
         }
-        if (ans > INT_MAX)
-            ans = INT_MAX;
-        else if (ans < INT_MIN)
-            ans = INT_MIN;
-        return ans;
+        if (neg)
+            result = -result;
+        result = result > INT_MAX ? INT_MAX : result;
+        result = result < INT_MIN ? INT_MIN : result;
+        return int(result);
     }
 };

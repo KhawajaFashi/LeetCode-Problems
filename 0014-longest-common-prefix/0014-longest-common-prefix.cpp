3945d689def04a484acbf07b84ce235546280638
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-07-27 12:02:27 +0500

    Time: 0 ms (100%), Space: 12 MB (33.35%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,33 +1,15 @@
-// class Solution {
-// public:
-//     string longestCommonPrefix(vector<string>& strs) {
-//         string res, ans = strs[0], result = ans;
-//         for (int i = 1; i < strs.size(); i++) {
-//             for (int j = 0; j < strs[i].length(); j++) {
-//                 if (ans[j] != strs[i][j])
-//                     break;
-//                 res += ans[j];
-//             }
-//             result = res;
-//             ans = res;
-//             res = "";
-//         }
-//         return result;
-//     }
-// };
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& v) {
-        string ans = "";
-        sort(v.begin(), v.end());
-        int n = v.size();
-        string first = v[0], last = v[n - 1];
-        for (int i = 0; i < min(first.size(), last.size()); i++) {
-            if (first[i] != last[i]) {
-                return ans;
-            }
-            ans += first[i];
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            string pre = "";
+            for (int j = 0; j < prefix.size() && j < strs[i].size() &&
+                            prefix[j] == strs[i][j]; j++)
+                pre += prefix[j];
+
+            prefix = pre.length() < prefix.length() ? pre : prefix;
         }
-        return ans;
+        return prefix;
     }
 };

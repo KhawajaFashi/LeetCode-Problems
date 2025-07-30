1154743415b6fde24ec27329f4fa3714fa9af4e7
Khawaja Fashi Ud Din Abdullah <112934339+KhawajaFashi@users.noreply.github.com>
2025-07-30 19:45:50 +0500

    Time: 0 ms (100%), Space: 13.2 MB (5.49%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,20 +12,12 @@
  */
 class Solution {
 public:
-    vector<int> Inorder(TreeNode* root, vector<int> ans) {
-        if (root == NULL)
-            return ans;
-        if (root->left == NULL && root->right == NULL) {
-            ans.push_back(root->val);
-            return ans;
-        }
-        ans = Inorder(root->left, ans);
-        ans.push_back(root->val);
-        ans = Inorder(root->right, ans);
-        return ans;
-    }
+    vector<int> arr;
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        return Inorder(root, ans);
+        if (root == NULL)
+            return arr;
+        arr = inorderTraversal(root->left);
+        arr.push_back(root->val);
+        return inorderTraversal(root->right);
     }
 };
